52a287fca55205f55cbb61cfff1ea0072d57b683
jd9913 <67403842+jd9913@users.noreply.github.com>
2021-01-08 13:50:41 -0700

    cart functionality created and tested

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -53,6 +53,43 @@ export const reducer = (state, action) => {
                 cart: [...state.cart, ...action.products],
             };
 
+        case REMOVE_FROM_CART:
+            let newState = state.cart.filter(product => {
+                return product._id !== action._id;
+            });
+
+            return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState
+            };
+
+        case UPDATE_CART_QUANTITY:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: state.cart.map(product => {
+                    if (action._id === product._id) {
+                        product.purchaseQuantity = action.purchaseQuantity;
+                    }
+                    return product;
+                })
+            };
+
+        case CLEAR_CART:
+            return {
+                ...state,
+                cartOpen: false,
+                cart: []
+            };
+
+            case TOGGLE_CART:
+                return{
+                    ...state,
+                    cartOpen:!state.cartOpen
+
+                };
+
 
         //if it's none of these actions, do not update state at all and keep things the same!
         default:
